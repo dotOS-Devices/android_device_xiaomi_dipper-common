243af734c107f999c07aac220ae901b1ef3432ac
TheScarastic <warabhishek@gmail.com>
2020-05-25 10:55:56 +0000

    sdm845-common: fod: Add surfaceflinger extension lib
    Change-Id: I51fb56c0a20e3cac83afb46ed6a6e2f967159d18

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
+# Fingerprint
+TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension.xiaomi_sdm845
+
 # Graphics
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
