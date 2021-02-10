1c48c2f8f7ab8b98b1fb1cdd98f12ed0d28c7b91
AdarshGrewal <adarshgrewal@gmail.com>
2021-02-10 08:12:43 +0530

    sdm845-common: overlay: adjust qqs for notch
    *remove and cleanup soon!

--- sdm845.mk
+++ sdm845.mk
@@ -23,7 +23,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
